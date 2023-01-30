01e225b6d9a1ae49b9cc1aa315c8d1d3a289856e
noruburi <rosss9238@gmail.com>
2023-01-30 07:55:45 +0000

    added get and post as well as request

--- nea 2/bank/website/auth.py
+++ nea 2/bank/website/auth.py
@@ -4,7 +4,7 @@ from flask_login import login_user, login_required, logout_user
 auth = Blueprint('auth', __name__) #defines auth blueprint to create url
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -13,6 +13,12 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login_user'))
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'post':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
     return render_template("sign_up.html", boolean=True)
