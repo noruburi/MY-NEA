9af49d2f399f0add32a9c6c20c79f953dfd4b480
noruburi <rosss9238@gmail.com>
2023-02-01 17:56:40 +0000

    improved score checker
    added missing requirements to my program which checks the password for any missing requirements and flashes an error message if there is

--- nea 2/bank/website/auth.py
+++ nea 2/bank/website/auth.py
@@ -13,6 +13,31 @@ def logout():
     logout_user()
     return redirect(url_for('auth.login_user'))
 
+
+def password_strength(password):
+    score = 0
+    if len(password) >= 8:
+        score += 1
+    if any(char.isdigit() for char in password):
+        score += 1
+    if any(char.isupper() for char in password):
+        score += 1
+    if any(char in set(r"!@#$%^&*()") for char in password):
+        score += 1
+    if score >= 4:
+        return score, "Password is strong."
+    else:
+        missing_requirements = []
+        if len(password) < 8:
+            missing_requirements.append("Password must be at least 8 characters.")
+        if not any(char.isdigit() for char in password):
+            missing_requirements.append("Password must contain at least one digit.")
+        if not any(char.isupper() for char in password):
+            missing_requirements.append("Password must contain at least one uppercase letter.")
+        if not any(char in set(r"!@#$%^&*()") for char in password):
+            missing_requirements.append("Password must contain at least one symbol (!@#$%^&*()).")
+        return score, ", ".join(missing_requirements)
+
 @auth.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
@@ -32,7 +57,11 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')            
         else:
-            flash('account created !', category='success')
+            score, message = password_strength(password1)
+            if score < 4:
+                flash(message, category='error')
+            else:
+                flash('account created !', category='success')
 
 
     return render_template("sign_up.html", boolean=True)
