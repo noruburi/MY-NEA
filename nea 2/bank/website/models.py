abfda150841f3dd91d6f54657dc2917b62322b6e
noruburi <rosss9238@gmail.com>
2023-02-02 00:44:55 +0000

    i did something

--- nea 2/bank/website/models.py
+++ nea 2/bank/website/models.py
@@ -1,6 +1,11 @@
 from . import db
 from flask_login import UserMixin
 
-class User(db.model, UserMixin):
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.coulmn(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(25))
+    first_name = db.Column(db.String(25))
+
