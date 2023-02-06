b3fa2672ee68d1d5d8589f0733145168bb5baca1
noruburi <rosss9238@gmail.com>
2023-02-06 14:58:26 +0000

    improved landing

--- nea 2/bank/website/auth.py
+++ nea 2/bank/website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db   #means from __init__.py import db
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -36,7 +36,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login_user'))
+    return redirect(url_for('auth.login'))
 
 
 def password_strength(password):
