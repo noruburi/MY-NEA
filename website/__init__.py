180d2d579d1aea6dcc36f5cd18bd0f943c371e42
noruburi <rosss9238@gmail.com>
2023-02-12 10:27:09 +0000

    fixed the database instantiation issue

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'#store database inside the directory of the init.py file
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///H:/Documents/GitHub/MY-NEA/website/' + DB_NAME
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_NAME
     db.init_app(app)
 
 
