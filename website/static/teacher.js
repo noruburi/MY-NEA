ead6f4b3054955ce4acb3696c57e35185cc01194
noruburi <rosss9238@gmail.com>
2023-03-18 10:14:13 +0000

    yeah we fixing this js

--- website/static/teacher.js
+++ website/static/teacher.js
@@ -1,32 +1,4 @@
-var ctx = document.getElementById('barChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Class 1', 'Class 2', 'Class 3', 'Class 4', 'Class 5', 'Class 6'],
-        datasets: [{
-            label: 'Points Awarded',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-//seperate function for pie chart 
-
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('pieChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'pie',
     data: {
