9f3f37f4c49a6ec226f1c07f8ca550ace550675f
noruburi <rosss9238@gmail.com>
2023-03-19 23:04:30 +0000

    money moves

--- website/auth.py
+++ website/auth.py
@@ -261,56 +261,7 @@ def sign_up():
 
 
 
-# @auth.route('/award_points', methods=['GET', 'POST'])
-# @login_required
-# def award_points():
-#     if current_user.is_admin() or current_user.is_teacher():
-#         students = User.query.filter_by(role_id=3).all()
-#         if request.method == 'POST':
-#             student_name = request.form['student_name']
-#             class_name = request.form['class_name']
-#             points = int(request.form['amount'])
-            
-#             # Check if teacher has enough remaining points to award
-#             if current_user.remaining_points < points:
-#                 flash('You do not have enough points to award.', 'danger')
-#                 return redirect(url_for('auth.award_points'))
-            
-#             # Check if teacher has exceeded weekly point limit
-#             if current_user.points_awarded_this_week >= current_user.weekly_point_limit:
-#                 flash('You have exceeded your weekly point limit.', 'danger')
-#                 return redirect(url_for('auth.award_points'))
-            
-#             student = User.query.filter_by(first_name=student_name).first()
-#             class_ = Class.query.filter_by(name=class_name, teacher=current_user).first()
-#             if student and class_:
-#                 if student.account:
-#                     student.account.balance += points
-#                     db.session.commit()
-#                     transaction = Transactions(
-#                         sequence=1,
-#                         from_account_id=current_user.id,
-#                         dateTime=datetime.utcnow(),
-#                         to_account_id=student.id,
-#                         amount=points
-#                     )
-#                     db.session.add(transaction)
-#                     db.session.commit()
-#                     flash('Transaction successful!', 'success')
-#                     return redirect(url_for('auth.award_points'))
-#                 else:
-#                     flash('Invalid student ID', 'danger')
-#             else:
-#                 flash('Invalid student name or class name', 'danger')
 
-#         # Calculate remaining points and percentage
-#         remaining_points = current_user.remaining_points
-#         remaining_point_percentage = current_user.remaining_point_percentage
-        
-#         # Render the template with the remaining points and percentage
-#         return render_template('award_points.html', students=students, user=current_user, remaining_points=remaining_points, remaining_point_percentage=remaining_point_percentage)
-
-   
 @auth.route('/award_points', methods=['GET', 'POST'])
 @login_required
 def award_points():
@@ -405,9 +356,9 @@ def create_class():
 
 
 
-@auth.route('/student')
+@auth.route('/student/<int:student_id>')
 @login_required
-def student():
+def student(student_id):
     student = User.query.filter_by(id=current_user.id, role_id=3).first()
     if student:
         search_year_group = request.args.get('year_group', type=int)
@@ -429,6 +380,7 @@ def student():
             
         subjects = Subject.query.all()
         teachers = User.query.filter_by(role_id=2).all()  # Fetch all teachers
+        join_requests = JoinRequest.query.filter_by(student_id=student_id).all()
         
         return render_template('student.html', student=student, classes=classes, user=current_user, search_year_group=search_year_group, search_subject_id=search_subject_id, search_teacher_id=search_teacher_id, subjects=subjects, teachers=teachers)
     else:
@@ -440,6 +392,12 @@ def student():
 
 @auth.route('/request_join_class/<int:student_id>/<int:class_id>', methods=['GET'])
 def request_join_class(student_id, class_id):
+    # Check if the user already has a join request for the class
+    existing_join_request = JoinRequest.query.filter_by(student_id=student_id, class_id=class_id).first()
+    if existing_join_request:
+        flash('You already have a join request for this class.', 'warning')
+        return redirect(url_for('auth.student', student_id=student_id))
+
     # Create a new join request object
     join_request = JoinRequest(student_id=student_id, class_id=class_id, status='pending')
     db.session.add(join_request)
@@ -449,33 +407,7 @@ def request_join_class(student_id, class_id):
     return redirect(url_for('auth.student', student_id=student_id))
 
 
-# @auth.route('/student_rewards', methods=['GET', 'POST'])
-# @login_required
-# def student_rewards():
-#     student = User.query.filter_by(id=current_user.id, role_id=3).first()
-#     if student:
-#         available_items = [
-#             {'name': 'Pen', 'description': 'A nice pen', 'points': 10},
-#             {'name': 'Notebook', 'description': 'A useful notebook', 'points': 20},
-#             {'name': 'Coffee', 'description': 'A cup of coffee', 'points': 30},
-#             {'name': 'Lunch', 'description': 'A delicious lunch', 'points': 50},
-#         ]
-#         if request.method == 'POST':
-#             item_index = int(request.form.get('item_index'))
-#             item = available_items[item_index]
-#             if current_user.remaining_points >= item['points']:
-#                 coupon_code = None  # TODO: Generate unique 8-digit code
-#                 coupon = Coupon(student_id=student.id, name=item['name'], description=item['description'], points_cost=item['points'], code=coupon_code, redeemed=False, redeem_date=None)
-#                 db.session.add(coupon)
-#                 db.session.commit()
-#                 flash('Coupon purchased successfully!', 'success')
-#             else:
-#                 flash('You do not have enough points to purchase this item', 'error')
-#     else:
-#         flash('You are not a student!', category='error')
-#         return redirect(url_for('views.home'))
-            
-#     return render_template('student_rewards.html', available_items=available_items, student=student, user=current_user)
+
 
 @auth.route('/student_rewards', methods=['GET', 'POST'])
 @login_required
