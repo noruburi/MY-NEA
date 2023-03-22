25c25bd18082941e22e520a27a611f19c551fb65
noruburi <67956642+noruburi@users.noreply.github.com>
2023-03-22 11:27:48 +0000

    Add files via upload

---
+++ MY-NEA-1.6-the-final-stretch/main.py
@@ -0,0 +1,12 @@
+from website import create_app
+from website import recreate_database
+from flask_wtf.csrf import CSRFProtect
+
+
+app = create_app()
+# csrf = CSRFProtect(app)
+
+if __name__ == '__main__':
+    # recreate_database(app)
+    app.run(debug=True)
+    
