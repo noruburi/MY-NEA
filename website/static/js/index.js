3416c03977745e06b3c4db01032cad5f5edfa12e
noruburi <rosss9238@gmail.com>
2023-09-14 11:16:07 +0100

    getting javascript to work (i hope)

---
+++ website/static/js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
+
+  window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 80);
+  });
+});
